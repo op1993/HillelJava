6a29146ea713a7ea3ec1410608631635e64d884f
op <plyska1993@gmail.com>
2018-11-14 23:50:20 +0200

    reformat code

--- src/main/java/FirstHomeWork.java
+++ src/main/java/FirstHomeWork.java
@@ -3,17 +3,25 @@ import java.util.Random;
 public class FirstHomeWork {
 
 
-    public int [][] fillArray(int sizeA ,int sizeB, int minValue, int maxValue){
-        int array [][] = new int[sizeA][sizeB];
-        for (int i =0; i < array.length; i++){
+    public static void main(String[] args) {
+        FirstHomeWork firstHomeWork = new FirstHomeWork();
+        int array[][] = firstHomeWork.fillArray(10, 10, 0, 100);
+        firstHomeWork.printValuesFromArray(array);
+
+        firstHomeWork.printNameAndSurname();
+    }
+
+    public int[][] fillArray(int sizeA, int sizeB, int minValue, int maxValue) {
+        int array[][] = new int[sizeA][sizeB];
+        for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                array [i][j] = new Random().nextInt(maxValue-minValue+1);
+                array[i][j] = new Random().nextInt(maxValue - minValue + 1);
             }
         }
         return array;
     }
 
-    public void printValuesFromArray(int array[][]){
+    public void printValuesFromArray(int array[][]) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
@@ -22,15 +30,7 @@ public class FirstHomeWork {
         }
     }
 
-    public static void main(String[] args) {
-        FirstHomeWork firstHomeWork =  new FirstHomeWork();
-        int array [][] = firstHomeWork.fillArray(10,10,0,100);
-        firstHomeWork.printValuesFromArray(array);
-
-        firstHomeWork.printNameAndSurname();
-    }
-
-    public void printNameAndSurname(){
+    public void printNameAndSurname() {
         System.out.println("*******   *******");
         System.out.println("**   **   **   **");
         System.out.println("**   **   **   **");
