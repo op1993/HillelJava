5cbd95a50a85843f10350e6c694221bff9ee625c
op <plyska1993@gmail.com>
2018-11-15 00:51:33 +0200

    Added asserts for negative cases

--- src/main/java/FirstHomeWork.java
+++ src/main/java/FirstHomeWork.java
@@ -5,13 +5,35 @@ public class FirstHomeWork {
 
     public static void main(String[] args) {
         FirstHomeWork firstHomeWork = new FirstHomeWork();
-        int array[][] = firstHomeWork.fillArray(10, 10, 0, 100);
+        int array[][] = firstHomeWork.fillArray(2, 100, 0, 1);
         firstHomeWork.printValuesFromArray(array);
 
         firstHomeWork.printNameAndSurname();
     }
 
     public int[][] fillArray(int sizeA, int sizeB, int minValue, int maxValue) {
+        if (sizeA <= 0) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                System.out.println("Value sizeA should be  >  0");
+            }
+        }
+        if (sizeB <= 0) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                System.out.println("Value sizeB should be  >  0");
+            }
+        }
+        if (minValue < 0) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                System.out.println("Min value should be  >=  0");
+            }
+        }
+
         int array[][] = new int[sizeA][sizeB];
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
